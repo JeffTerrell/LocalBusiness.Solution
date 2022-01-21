bd354106f021ac313fee4faf0d17ecba4bf98e63
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-21 13:22:59 -0500

    create controller with GET, GET(id), and POST routes

---
+++ LocalBusiness/Controllers/BusinessesController.cs
@@ -0,0 +1,50 @@
+using LocalBusiness.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace LocalBusiness.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class BusinessesController : ControllerBase
+  {
+    private readonly LocalBusinessContext _db;
+
+    public BusinessesController(LocalBusinessContext db)
+    {
+      _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Business>>> Get()
+    {
+      return await _db.Businesses.ToListAsync();
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<Business>> GetBusiness(int id)
+    {
+      var business = await _db.Businesses.FindAsync(id);
+      if (business == null)
+      {
+        return NotFound();
+      }
+      return business;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Business>> Post(Business business)
+    {
+      _db.Businesses.Add(business);
+      await _db.SaveChangesAsync();
+
+      return CreatedAtAction(nameof(GetBusiness), new { id = business.BusinessId}, business);
+    }
+
+  }
+}
