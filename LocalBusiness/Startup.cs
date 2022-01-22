0f639afa174875626a75aa700ccab6300651a7b6
Jeff Terrell <jeffaterrell@gmail.com>
2022-01-21 19:06:54 -0500

    update Startup.cs to invoke api-version in HTTP header

--- LocalBusiness/Startup.cs
+++ LocalBusiness/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -33,6 +34,7 @@ namespace LocalBusiness
                 config.DefaultApiVersion = new ApiVersion(1, 0);
                 config.AssumeDefaultVersionWhenUnspecified = true;
                 config.ReportApiVersions = true;
+                config.ApiVersionReader = new HeaderApiVersionReader("api-version");
             });
 
             services.AddSwaggerGen(options =>
